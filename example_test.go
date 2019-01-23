b244e1a7355ac6a5b26cf0bb90f71743e0fd7201
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2019-01-23 08:25:02 +0000

    Added GetAllGroups to examples and moved the client setup to its own function

--- example_test.go
+++ example_test.go
@@ -8,18 +8,7 @@ import (
 
 // ExampleLDAPClient_Authenticate shows how a typical application can verify a login attempt
 func ExampleLDAPClient_Authenticate() {
-	client := &ldap.LDAPClient{
-		Base:         "dc=example,dc=com",
-		Host:         "ldap.example.com",
-		Port:         389,
-		UseSSL:       false,
-		BindDN:       "uid=readonlysuer,ou=People,dc=example,dc=com",
-		BindPassword: "readonlypassword",
-		UserFilter:   "(uid=%s)",
-		GroupFilter:  "(memberUid=%s)",
-		Attributes:   []string{"givenName", "sn", "mail", "uid"},
-	}
-	defer client.Close()
+	client := setupClient()
 
 	ok, user, err := client.Authenticate("username", "password")
 	if err != nil {
@@ -34,13 +23,8 @@ func ExampleLDAPClient_Authenticate() {
 
 // ExampleLDAPClient_GetGroupsOfUser shows how to retrieve user groups
 func ExampleLDAPClient_GetGroupsOfUser() {
-	client := &ldap.LDAPClient{
-		Base:        "dc=example,dc=com",
-		Host:        "ldap.example.com",
-		Port:        389,
-		GroupFilter: "(memberUid=%s)",
-	}
-	defer client.Close()
+	client := setupClient()
+
 	groups, err := client.GetGroupsOfUser("username")
 	if err != nil {
 		log.Fatalf("Error getting groups for user %s: %+v", "username", err)
@@ -50,16 +34,38 @@ func ExampleLDAPClient_GetGroupsOfUser() {
 
 // ExampleLDAPClient_GetAllUsers shows how to retrieve all users
 func ExampleLDAPClient_GetAllUsers() {
-	client := &ldap.LDAPClient{
-		Base:        "dc=example,dc=com",
-		Host:        "ldap.example.com",
-		Port:        389,
-		GroupFilter: "(memberUid=%s)",
-	}
-	defer client.Close()
+	client := setupClient()
+
 	users, err := client.GetAllUsers("sAMAccountName")
 	if err != nil {
 		log.Fatalf("Error getting users: %+v", err)
 	}
 	log.Printf("Users: %+v", users)
+}
+
+// ExampleLDAPClient_GetAllGroups shows how to retrieve all groups
+func ExampleLDAPClient_GetAllGroups() {
+	client := setupClient()
+
+	groups, err := client.GetAllGroups()
+	if err != nil {
+		log.Fatalf("Error getting groups: %+v", err)
+	}
+	log.Printf("Groups: %+v", groups)
+}
+
+func setupClient() *ldap.LDAPClient {
+	client := &ldap.LDAPClient{
+		Base:         "dc=example,dc=com",
+		Host:         "ldap.example.com",
+		Port:         389,
+		UseSSL:       false,
+		BindDN:       "uid=readonlysuer,ou=People,dc=example,dc=com",
+		BindPassword: "readonlypassword",
+		UserFilter:   "(uid=%s)",
+		GroupFilter:  "(memberUid=%s)",
+		Attributes:   []string{"givenName", "sn", "mail", "uid"},
+	}
+	defer client.Close()
+	return client
 }
